d4eb4e4c75e9b8ab4a2017f10cd1387aa798fff1
pbetb3370 <73011783+pbetb3370@users.noreply.github.com>
2024-07-13 22:58:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+const express = require("express");
+const app = express();
+app.set("view engine","hbs");
+app.use(express.static("static"));
+app.use(express.urlencoded({extended:true}))
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.post("/",(req,res)=>{
+    if(req.body.user==process.env.USER&&req.body.password==process.env.PASSWORD){
+        res.render("main_dashboard")
+    }else{
+        res.render("index",{error:true,message:"User or Password not matched!"})
+    }
+})
+
+
+app.listen(3000||process.env.PORT)
