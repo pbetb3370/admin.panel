a0f901f085c7d68df93d37bf7799049ef75d6f55
pbetb3370 <73011783+pbetb3370@users.noreply.github.com>
2024-07-21 10:45:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 require("dotenv").config()
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser")
+const SECRET = "Hello this is a super secret code dont edit. this will be use for authentication and security";
 app.set("view engine","hbs");
 app.use(express.static("static"));
 app.use(express.urlencoded({extended:true}))
-app.get("/",(req,res)=>{
-    res.render("index")
+app.use(cookieParser())
+async function protected(req,res,next){
+    try{
+        let cki = req.cookies.jwt;
+        await jwt.verify(cki,SECRET);
+        next();
+    }catch(err){
+        res.render("index",{error:true,message:"Login to continue"})
+    }
+}
+app.get("/",protected,(req,res)=>{
+    res.render("main_dashboard")
 })
-
-app.post("/",(req,res)=>{
+app.get("/logout",(req,res)=>{
+    res.clearCookie("jwt");
+    res.redirect("/")
+})
+app.post("/",async(req,res)=>{
     if(req.body.user==process.env.USER&&req.body.password==process.env.PASSWORD){
+        try{
+        let tkn = await jwt.sign({user:req.body.user},SECRET,{
+            expiresIn:"7d"
+        })
+        res.cookie("jwt",tkn,{httpOnly:true})
         res.render("main_dashboard")
+        }catch(err){
+            res.render("index",{error:true,message:"Failed to authenticate user"})
+        }
     }else{
         res.render("index",{error:true,message:"User or Password not matched!"})
     }
 })
 
 
+app.get("/certificate",protected,(req,res)=>{
+    res.render("certificate")
+})
+app.get("/institute",protected,(req,res)=>{
+    res.render("institute")
+})
+app.get("/portal",protected,(req,res)=>{
+    res.render("portal")
+})
+app.get("/result",protected,(req,res)=>{
+    res.render("result")
+})
+app.get("/tssc",protected,(req,res)=>{
+    res.render("tssc")
+})
+app.get("/admin",protected,(req,res)=>{
+    res.render("admin")
+})
 app.listen(3000||process.env.PORT)
